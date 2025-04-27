25e62ea3109aa8aa12b727074e53d9b82d15c7d5
mlnls <tmdals140@naver.com>
2025-04-27 21:47:51 +0900

    완주하지 못한 사람 solved: #3
    정확성: 58.3
    효율성: 41.7
    합계: 100.0 / 100.0

--- algoKIT/hash/level_1.cpp
+++ algoKIT/hash/level_1.cpp
@@ -1,22 +1,26 @@
 // 정확성: 58.3
-// 효율성: 0.0
-// 합계: 58.3 / 100.0
+// 효율성: 41.7
+// 합계: 100.0 / 100.0
 
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 string solution(vector<string> participant, vector<string> completion) {    
-    for(int j=0; j<completion.size(); j++){
-        for(int i=0; i<participant.size(); i++) {
-            if(completion.at(j) == participant.at(i)) {
-                participant.erase(participant.begin()+i); 
-                break;
-            }
-        } 
+    sort(participant.begin(), participant.end());
+    sort(completion.begin(), completion.end());
+    
+    string answer = "";
+    
+    for(int i=0; i<completion.size(); i++){
+        if(participant.at(i) != completion.at(i)) {
+            answer = participant.at(i);
+            return answer;
+        }
     }
     
-    string answer = participant.front();
+    answer = participant.back();
     return answer;
 }
