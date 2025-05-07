1cb98b9e726b20f2d79c0f81bfd6631ec63bba09
mlnls <tmdals140@naver.com>
2025-05-08 00:17:28 +0900

    전화번호 목록
    feat: #7
    
    정확성: 83.3
    효율성: 16.7
    합계: 100.0 / 100.0

--- algoKIT/hash/level_3.cpp
+++ algoKIT/hash/level_3.cpp
@@ -13,12 +13,11 @@ bool solution(vector<string> phone_book) {
     bool answer = true;
     sort(phone_book.begin(), phone_book.end());
 
-    while(!phone_book.empty()){        
-        if(phone_book[0] == phone_book[1].substr(0,phone_book[0].size())) {
-            return false;
+    for(int i=0; i<phone_book.size()-1; i++){
+        if(phone_book[i] == phone_book[i+1].substr(0,phone_book[i].size())) {
+            answer = false;
+            return answer;
         }
-        
-        phone_book.erase(phone_book.begin());
     }
     
     return answer;
